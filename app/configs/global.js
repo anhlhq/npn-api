d3badc6e623f3913df34ddae024b3cd428079002
lehuyquanganh <lehuyquanganh00@gmail.com>
2022-08-28 18:14:14 +0700

    update

--- app/configs/global.js
+++ app/configs/global.js
@@ -1,2 +1,4 @@
 global.__SALT_WORK_FACTOR = 10;
 global.__JWT_SECRET = "eyJkZXZlbG9wZXIiOiJsZWh1eXF1YW5nYW5oIn0";
+global.DB_NAME = "npn-api";
+global.SSH_DB_ENABLED = false;
--- app/configs/mongoose.js
+++ app/configs/mongoose.js
@@ -1,11 +1,36 @@
 const mongoose = require("mongoose");
+const tunnel = require("tunnel-ssh");
+const fs = require("fs");
+
+const connectTunnel = () => {
+  var config = {
+    host: "sv.quanganh.uk",
+    port: "22",
+    username: "cloud",
+    dstHost: "127.0.0.1",
+    dstPort: "27017",
+    localHost: "127.0.0.1",
+    localPort: "27000",
+    privateKey: fs.readFileSync("/home/lhqa/.ssh/id_rsa"),
+  };
+
+  return new Promise((resolve, reject) => {
+    tunnel(config, (error, server) => {
+      if (error) {
+        console.log("SSH connection error: " + error);
+        return reject(error);
+      }
+      resolve(server);
+    });
+  });
+};
 
 const connectMongo = () =>
   new Promise((resolve, reject) => {
-    let db =
-      process.env.NODE_ENV !== "production"
-        ? "mongodb://127.0.0.1:27017/npn-api"
-        : process.env.MONGO_URI;
+    var db = "mongodb://127.0.0.1:27017/" + DB_NAME;
+    if (process.env.NODE_ENV != "production" && SSH_DB_ENABLED) {
+      db = "mongodb://127.0.0.1:27000/" + DB_NAME;
+    }
     mongoose.connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -18,6 +43,9 @@ const connectMongo = () =>
 
 exports.connect = async () => {
   try {
+    if (process.env.NODE_ENV !== "production" && SSH_DB_ENABLED) {
+      await connectTunnel();
+    }
     await connectMongo();
     console.log("MongoDB connected");
   } catch (error) {
