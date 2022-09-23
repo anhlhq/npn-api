b512447d25a5f2428fc414da0133bbbbfc19553c
Le Huy Quang Anh <lehuyquanganh00@gmail.com>
2022-09-23 11:26:05 +0700

    update

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
-const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const flash = require("connect-flash");
 const compression = require("compression");
@@ -20,7 +19,6 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(flash());
 app.use(compression());
