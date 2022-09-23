846800c52213826a56dbc115dbcdac2f59d21db1
Le Huy Quang Anh <lehuyquanganh00@gmail.com>
2022-09-23 11:02:23 +0700

    update

--- app.js
+++ app.js
@@ -23,6 +23,14 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(flash());
 app.use(compression());
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use("/", routers);
 // catch 404 and forward to error handler
