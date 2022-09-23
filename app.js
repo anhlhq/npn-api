a847bc82bf9cc5de4365e6642d68cc39b0b3cfdc
Le Huy Quang Anh <lehuyquanganh00@gmail.com>
2022-09-23 16:04:57 +0700

    update

--- app.js
+++ app.js
@@ -24,11 +24,14 @@ app.use(flash());
 app.use(compression());
 app.use(cors({ credentials: true, origin: "*" }));
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://nhanphatnhanh.com");
-  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Origin", "*)");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+  );
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
   );
   next();
 });
