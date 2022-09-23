0c708c030cee740cb9b60e4cf554a4e0a61d031a
Le Huy Quang Anh <lehuyquanganh00@gmail.com>
2022-09-23 15:33:41 +0700

    uodate

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(compression());
 app.use(cors({ credentials: true, origin: "*" }));
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://nhanphatnhanh.com");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Methods", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
