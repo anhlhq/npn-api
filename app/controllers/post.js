b36a9fcba264c7195a7aef9b2da6a4b2d130579b
Le Huy Quang Anh <lehuyquanganh00@gmail.com>
2022-09-23 10:29:30 +0700

    update

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -3,7 +3,7 @@ const Post = require("../models/Post");
 
 exports.getAll = async (req, res) => {
   let type = req.query.type ?? "post";
-  let limit = req.query.limit === "infinity" ? Infinity : 4;
+  let limit = req.query.limit;
   let page = req.query.page ?? 1;
   let skip = limit * (page - 1);
 
