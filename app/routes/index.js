3238255b532525ee60f59dc7a04ad1142e377474
Anh Le <eric.anhlhv@gmail.com>
2022-09-25 13:50:40 +0700

    update

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,17 @@
 var express = require("express");
+const cors = require("cors");
 var router = express.Router();
+
+var corsOptions = {
+  origin: "https://nhanphatnhanh.com",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
-router.use("/api", require("./api"));
+router.use("/api", cors(corsOptions), require("./api"));
 
 module.exports = router;
